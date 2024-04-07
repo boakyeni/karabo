898527cfa3d42709e5dcaf50c007e20d41fda133
Kojo <boakyeni@usc.edu>
2024-04-06 19:01:19 -0700

    auth urls added

--- karabo/urls.py
+++ karabo/urls.py
@@ -18,4 +18,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
-urlpatterns = [path("admin/", admin.site.urls), path("", include("landing.urls"))]
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("", include("landing.urls")),
+    path("", include("auth.urls")),
+]
